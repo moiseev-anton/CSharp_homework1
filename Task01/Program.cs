2ce62198bf2972d88d24318a680edae6f25253a0
anton moiseev <moucee13@gmail.com>
2023-01-27 13:50:25 +0300

    Решил вторую задачу

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Clear();
+﻿// ЗАДАЧА 2: Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
 Console.WriteLine("Введите два целых числа.");
 Console.Write("Первое число: ");
 int number1 = int.Parse(Console.ReadLine());
@@ -6,11 +12,11 @@ Console.Write("Второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 
 if (number1 > number2)
-    Console.WriteLine($"Max={number1}");
+    Console.WriteLine($"Max={number1} Min={number2}");
 else
 {
     if (number1 < number2)
-        Console.WriteLine($"Max={number2}");
+        Console.WriteLine($"Max={number2} Min={number1}");
     else
         Console.WriteLine("Числа равны");
 }
