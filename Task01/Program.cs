9081d056b908a17dc3a843f1bef5c6923564fdb6
anton moiseev <moucee13@gmail.com>
2023-01-27 13:00:30 +0300

    Решил первую задачу

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите два целых числа.");
+Console.Write("Первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+    Console.WriteLine($"Max={number1}");
+else
+{
+    if (number1 < number2)
+        Console.WriteLine($"Max={number2}");
+    else
+        Console.WriteLine("Числа равны");
+}
